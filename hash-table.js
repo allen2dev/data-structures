ea64e766f8fbb2e3036c5c35546261deb96168c9
zhiqiuyao <nqxllc2014@gmail.com>
2017-01-06 15:42:30 +0800

    完成数据结构Hash Table 包含add, remove, search, print

---
+++ hash-table.js
@@ -0,0 +1,71 @@
+/**
+ * 定义：
+ * Hash Table (Hash Map) 是用于实现关联数组的数据结构，该结构可以将键映射到值，
+ * Hash Table 使用哈希函数将索引计算到 buckets(桶) 或 slots(插槽) 数组中，从中可以找到所需的值
+ * */
+
+function HashTable(size) {
+  this.values = {};
+  this.numberOfValues = 0;
+  this.size = size;
+}
+
+HashTable.prototype.add = function(key, value) {
+  var hash = this.calculateHash(key);
+  if(!this.values.hasOwnProperty(hash)) {
+    this.values[hash] = {};
+  }
+  if(!this.values[hash].hasOwnProperty(key)) {
+    this.numberOfValues++;
+  }
+  this.values[hash][key] = value;
+};
+
+HashTable.prototype.remove = function(key) {
+  var hash = this.calculateHash(key);
+  if(this.values.hasOwnProperty(hash) && this.values[hash].hasOwnProperty(key)) {
+    delete this.values[hash][key];
+    this.numberOfValues--;
+  }
+};
+
+HashTable.prototype.calculateHash = function(key) {
+  return key.toString().length % this.size;
+};
+
+HashTable.prototype.search = function(key) {
+  var hash = this.calculateHash(key);
+  if(this.values.hasOwnProperty(hash) && this.values[hash].hasOwnProperty(key)) {
+    return this.values[hash][key];
+  } else {
+    return null;
+  }
+};
+
+HashTable.prototype.length = function() {
+  return this.numberOfValues;
+};
+
+HashTable.prototype.print = function() {
+  var string = '';
+  for(var value in this.values) {
+    for(var key in this.values[value]) {
+      string += this.values[value][key] + ' ';
+    }
+  }
+  console.log(string.trim());
+};
+
+var hashTable = new HashTable(3);
+hashTable.add('first', 1);
+hashTable.add('second', 2);
+hashTable.add('third', 3);
+hashTable.add('fourth', 4);
+hashTable.add('fifth', 5);
+hashTable.print(); // => 2 4 1 3 5
+console.log('返回Hash Table长度:', hashTable.length()); // => 5
+console.log('搜索key 为 "second" 的值:', hashTable.search('second')); // => 2
+hashTable.remove('fourth');
+hashTable.remove('first');
+hashTable.print(); // => 2 3 5
+console.log('返回Hash Table长度:', hashTable.length()); // => 3
